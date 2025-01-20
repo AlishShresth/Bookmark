9740329779349ace2c2c73a4f6c58ca9f02407c8
Alish Shrestha <cresthaalish@gmail.com>
2025-01-20 21:52:27 +0545

    Using the message framework

--- account/views.py
+++ account/views.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import (
-    LoginForm, 
-    UserRegistrationForm, 
-    UserEditForm, 
+    LoginForm,
+    UserRegistrationForm,
+    UserEditForm,
     ProfileEditForm
 )
 from .models import Profile
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -31,6 +33,7 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
 
 def register(request):
     if request.method == 'POST':
@@ -57,6 +60,7 @@ def register(request):
         {'user_form': user_form}
     )
 
+
 @login_required
 def dashboard(request):
     return render(
@@ -65,21 +69,31 @@ def dashboard(request):
         {'section': 'dashboard'}
     )
 
+
 @login_required
 def edit(request):
     if request.method == 'POST':
         user_form = UserEditForm(
-            instance = request.user,
-            data = request.POST
+            instance=request.user,
+            data=request.POST
         )
         profile_form = ProfileEditForm(
-            instance = request.user.profile,
-            data = request.POST,
-            files = request.FILES
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
         )
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(
+                request,
+                'Profile updated successfully'
+            )
+        else:
+            messages.error(
+                request,
+                'Error updating your profile'
+            )
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
